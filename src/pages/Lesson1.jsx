ae170b0767121a64bf77f1922cdfb7161ca339c2
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-12-11 22:45:13 +0800

    🦄 added the lesson1 page

--- src/pages/Lesson1.jsx
+++ src/pages/Lesson1.jsx
@@ -1,7 +1,19 @@
+import back from '../assets/back.svg';
+import forward from '../assets/forward.svg';
+import { Link } from 'react-router-dom';
+
 export default function Lesson1() {
   return(
     <>
+    <div className="lesson1-main">
+      
+       
+      <Link to="/"><button className="lesson1-prev"><img src={back} alt="Previous" className='lesson1-ico' /></button></Link>
+      <div className="lesson1-title">Lesson 1</div>
+     <button className="lesson1-forward"><img src={forward} alt="Forward" className='lesson1-ico' /></button>
     
+      
+    </div>
     </>
   );
 }
