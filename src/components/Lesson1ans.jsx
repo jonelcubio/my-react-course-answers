adbfcc576765987c7d0272002ef1a592d6eb6c4c
Jonelle Cubio <girlthatloveswriting@gmail.com>
2025-12-26 19:35:10 +0800

    🦄 answered the lesson 1 pt 1

---
+++ src/components/Lesson1ans.jsx
@@ -0,0 +1,56 @@
+export default function Lesson1ans() {
+  return(
+  <>
+  <main className="lesson1">
+    <div className="titleName">React Basic, JSX</div>
+    <div className="titlebody">
+
+      <div className="titlenum">1a.</div>
+      <div className="lessonbody">
+        <button className="lesson1btn">Good job!</button>
+      </div>
+
+      <div className="titlenum">1b.</div>
+      <div className="lessonbody">
+        <h1>My name is Jonelle</h1>
+      </div>
+
+      <div className="titlenum">1c.</div>
+      <div className="lessonbody">
+        <p>Cotton socks</p>
+        <p>Price: $10</p>
+        <button className="lesson1btn">Add to cart</button>
+      </div>
+
+      <div className="titlenum">1d.</div>
+      <div className="lessonbody">
+        <p>const productCost = 10 + 8 * 2;</p>
+        <p>console.log(productCost);</p>
+      </div>
+
+      <div className="titlenum">1e.</div>
+      <div className="lessonbody">
+        <p>const productCost = 10 + 8 * 2;</p>
+        <p>const paragraph = Product cost: $productCost;</p>
+      </div>
+
+      <div className="titlenum">1f.</div>
+      <div className="lessonbody">
+         <p>const productCost = 10 + 8 * 2;</p>
+         <p>const shippingCost = 5;</p>
+         <p>const totalCost = productCost + shippingCost;</p>
+          <br />
+         <p>Product cost: $productCost</p>
+         <p>Shipping cost: $shippingCost</p>
+         <p>Total cost: $totalCost</p>
+         <button className="lesson1btn">Place your order</button>
+      </div>
+
+        <br />
+        <br />
+      <p>Timestamp: continue on 40:01 on YT video</p>
+    </div>
+  </main>
+  </>
+  );
+}
--- src/pages/Lesson1.jsx
+++ src/pages/Lesson1.jsx
@@ -1,6 +1,7 @@
 import back from '../assets/back.svg';
 import forward from '../assets/forward.svg';
 import { Link } from 'react-router-dom';
+import Lesson1ans from '../components/Lesson1ans';
 
 export default function Lesson1() {
   return(
@@ -9,11 +10,15 @@ export default function Lesson1() {
       
        
       <Link to="/"><button className="lesson1-prev"><img src={back} alt="Previous" className='lesson1-ico' /></button></Link>
+
       <div className="lesson1-title">Lesson 1</div>
+      
      <button className="lesson1-forward"><img src={forward} alt="Forward" className='lesson1-ico' /></button>
     
-      
     </div>
+
+    <Lesson1ans />
+    
     </>
   );
 }
